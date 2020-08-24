89829ab07ca189b6dd36f4a66d0bde76b80cfe2a
Jacqueline Maasch <razel@Jacquelines-MacBook.local>
2020-08-24 14:30:50 -0400

    finalize sea palettes

--- R/earth.R
+++ R/earth.R
@@ -9,19 +9,19 @@ earth <- list(
             "#B89284FF", "#C9ADA3FF", "#DBC8C1FF", "#EDE3E0FF"),
   "3.8" = c("#6A3F48FF", "#7C575EFF", "#8F6F75FF", "#A1878CFF", 
             "#B49FA3FF", "#C7B7BAFF", "#D9CFD1FF", "#ECE7E8FF"),
-  "4.8" = c("#849895FF", "#93A4A2FF", "#A2B1AFFF", "#B2BEBCFF", 
-            "#C1CBCAFF", "#D0D8D7FF", "#E0E5E4FF", "#EFF2F1FF"),
+  "4.8" = c("#3D3B39FF", "#555351FF", "#6D6B6AFF", "#858483FF",
+            "#9D9C9BFF", "#B5B5B4FF", "#CDCDCDFF", "#E6E6E6FF"),
   "5.8" = c("#746E6DFF", "#85807FFF", "#969291FF", "#A8A4A3FF", 
             "#B9B6B5FF", "#CAC8C8FF", "#DCDADAFF", "#EDECECFF"),
   "1.5" = c("#410005FF", "#673337FF", "#8D6669FF", "#B3999BFF", "#D9CCCDFF"),
   "2.5" = c("#71260AFF", "#8D513BFF", "#A97C6CFF", "#C6A89DFF", "#E2D3CEFF"),
   "3.5" = c("#6A3F48FF", "#87656CFF", "#A58B91FF", "#C3B2B5FF", "#E1D8DAFF"),
-  "4.5" = c("#849895FF", "#9CACAAFF", "#B5C1BFFF", "#CDD5D4FF", "#E6EAE9FF"),
+  "4.5" = c("#3D3B39FF", "#676564FF", "#91908FFF", "#BBBBBAFF", "#E6E6E6FF"),
   "5.5" = c("#746E6DFF", "#8F8B8AFF", "#ABA8A7FF", "#C7C5C4FF", "#E3E2E1FF"),
   "1.3" = c("#410005FF", "#805558FF", "#BFAAABFF"),
   "2.3" = c("#71260AFF", "#A06E5BFF", "#CFB6ADFF"),
   "3.3" = c("#6A3F48FF", "#9B7F84FF", "#CDBFC2FF"),
-  "4.3" = c("#849895FF", "#ADBAB8FF", "#D6DCDBFF"),
+  "4.3" = c("#3D3B39FF", "#91908FFF", "#E6E6E6FF"),
   "5.3" = c("#746E6DFF", "#A29E9DFF", "#D0CECEFF")
 )
 
